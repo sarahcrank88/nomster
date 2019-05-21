5568f2e73313c9fe41fa5d184bded5da38b7c64f
Sarah Crank <sarahcrank88@gmail.com>
2019-05-21 00:29:57 +0000

    Lesson 10-39: Add an image pt 1

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,12 +3,13 @@ class PhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
+    @place.photos.create(photo_params)
     redirect_to place_path(@place)
   end
-  
-    private
+
+  private
 
   def photo_params
-    params.require(:photo).permit(:photo, :caption)
+    params.require(:photo).permit(:picture, :caption)
   end
 end
