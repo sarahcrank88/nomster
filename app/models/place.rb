c583f014317cc0d1bb31eebf9a3201822ec728b7
Sarah Crank <sarahcrank88@gmail.com>
2019-05-26 23:00:41 +0000

    trying to debug the user page, re: name

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,7 +6,7 @@ class Place < ApplicationRecord
   geocoded_by :address
   after_validation :geocode
   
-  validates :name, presence: true, length: {  minimum: 3}
+  validates :name, presence: true, length: { minimum: 3 }
   validates :address, presence: true
   validates :description, presence: true
 end
